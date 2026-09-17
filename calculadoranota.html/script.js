cfe35b6139d6eba4806059bd134b2de422fb3310
rogerdemelo <roger.braga.santos@escola.pr.gov.br>
2026-09-17 09:41:51 -0300

    nova atividade de calcular a nota gamer

---
+++ calculadoranota.html/script.js
@@ -0,0 +1,10 @@
+let nota1tri
+let nota2tri
+let resultado
+
+function calcular() {
+    nota1tri = Number (prompt(" digam-me a nota do primeiro trimestre"));
+    nota2tri = Number (prompt("digame a nota do segundo trimestre"));
+
+    resultado = 180 - nota1tri + nota2tri;
+}
