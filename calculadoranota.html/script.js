2026364ac678aba0163246741a9e1d8f809c1af1
rogerdemelo <roger.braga.santos@escola.pr.gov.br>
2026-09-24 08:05:27 -0300

    consertando a caluladora

--- calculadoranota.html/script.js
+++ calculadoranota.html/script.js
@@ -6,5 +6,9 @@ function calcular() {
     nota1tri = Number (prompt(" digam-me a nota do primeiro trimestre"));
     nota2tri = Number (prompt("digame a nota do segundo trimestre"));
 
-    resultado = 180 - nota1tri + nota2tri;
+    resultado = 180 - (nota1tri + nota2tri);
+
+    alert(resultado)
+
+    if(resultado){}
 }
